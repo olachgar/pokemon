d4f099e6a90e354754f8ef49dd542cb11d329988
olachgar <olachgar@assurone-group.com>
2018-01-02 17:23:48 +0100

    https://github.com/olachgar/pokemon.git

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from "@angular/router";
 
 import { AppComponent } from './app.component';
+import { PokemonDetailComponent } from './pokemons/pokemon-detail/pokemon-detail.component';
+import { PokemonListComponent } from './pokemons/pokemon-list/pokemon-list.component';
+
+
+const routes: Routes = [
+  { path: "", component: PokemonListComponent }
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonDetailComponent,
+    PokemonListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  exports:[RouterModule],
+  bootstrap: [AppComponent, PokemonListComponent]
 })
 export class AppModule { }
